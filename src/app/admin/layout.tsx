785a798e97f530fd11c971fb4ac201dd422f996a
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2025-03-03 08:39:51 +0700

    admin

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -31,10 +31,10 @@ export default function Layout({ children }: { children: ReactNode }) {
         <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
           {/* Sidebar content here */}
           <li>
-            <a>Sidebar Item 1</a>
+            <a href='/admin'>Dashboard</a>
           </li>
           <li>
-            <a>Sidebar Item 2</a>
+            <a href='/admin/device'>Devices</a>
           </li>
         </ul>
       </div>
